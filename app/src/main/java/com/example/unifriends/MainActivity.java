d33c452c9b570a967c86dfb9e8253ac0350c121e
Chih-Kuan Wu <c.wu35@student.unimelb.edu.au>
2019-10-17 14:13:22 +1100

    app is FIXED and somewhat working again

--- app/src/main/java/com/example/unifriends/MainActivity.java
+++ app/src/main/java/com/example/unifriends/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import com.example.unifriends.dashboard.Dashboard;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
