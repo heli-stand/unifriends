04b687fc2eb8402e32cc632b9a8f9fe7013ac93c
Li He <jerelyhe@gmail.com>
2019-10-21 18:37:35 +1100

    facial search done, tested

--- app/src/main/java/com/example/unifriends/FacialSearch.java
+++ app/src/main/java/com/example/unifriends/FacialSearch.java
@@ -1,5 +1,6 @@
 package com.example.unifriends;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -19,15 +20,24 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.microsoft.projectoxford.face.FaceServiceClient;
 import com.microsoft.projectoxford.face.FaceServiceRestClient;
 import com.microsoft.projectoxford.face.contract.Face;
 import com.microsoft.projectoxford.face.contract.FaceRectangle;
+import com.microsoft.projectoxford.face.contract.VerifyResult;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DecimalFormat;
 import java.util.UUID;
 
 public class FacialSearch extends AppCompatActivity {
@@ -35,6 +45,7 @@ public class FacialSearch extends AppCompatActivity {
     private final int PICK_IMAGE = 1;
     private final String apiEndpoint = "https://tryface.cognitiveservices.azure.com/face/v1.0";
     private final String subscriptionKey = "40fef214516e4321bff17da500858306";
+    private final String TAG = "Facial Search";
     private final FaceServiceClient faceServiceClient =
             new FaceServiceRestClient(apiEndpoint, subscriptionKey);
     private Face[] faces;
@@ -59,6 +70,7 @@ public class FacialSearch extends AppCompatActivity {
 //
                     if (faceClicked(face.faceRectangle,  Math.floor(pts[0]), Math.floor(pts[1]))){
                         Log.d("Face touched", face.faceId.toString());
+
                         searchFace(face.faceId);
                         break;
                     }
@@ -222,8 +234,130 @@ public class FacialSearch extends AppCompatActivity {
         return bitmap;
     }
 
-    private void searchFace(UUID faceId){
+    private void searchFace(final UUID faceId){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+        db.collection("users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+//                    QuerySnapshot document = task.getResult();
+                    if (!task.getResult().isEmpty()) {
+                        boolean macthed = false;
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+//                            String uuidProfile = document.get("facialID").toString();
+//                            final UUID uuidProfile = UUID.fromString(getIntent().getStringExtra("facialID"));
+                            if (verify( document.get("facialID").toString(), faceId)){
+                                Log.d(TAG, "Found, userId: " + document.getId() );
+                                Intent intent = new Intent(FacialSearch.this, Profile.class);
+                                intent.putExtra("userID", document.getId());
+                                startActivity(intent);
+                                macthed = true;
+                                break;
+                            }
+                        }
+                        Log.d(TAG, "Search Finished" );
+                        if (!macthed){
+                            Log.d(TAG, "Sorry this person is not registered" );
+                        }
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+    }
+
+    private boolean verify(String uuidProfile, UUID faceID){
+        final UUID selected = faceID;
+        final UUID profilePhoto = UUID.fromString(uuidProfile);
+
+//        try  {
+//            //Your code goes here
+//            VerifyResult result = faceServiceClient.verify(selected, profilePhoto);
+//            DecimalFormat formatter = new DecimalFormat("#0.00");
+//            String verificationResult = (result.isIdentical ? "The same person": "Different persons")
+//                    + ". The confidence is " + formatter.format(result.confidence);
+//            Log.d(TAG, verificationResult);
+//
+//            if (result.isIdentical){
+//                Log.d(TAG, "Identical Person");
+//                return true;
+//            }else{
+//                Log.d(TAG, "Log In Failed.Different Person");
+//                return false;
+//            }
+//
+////                    TextView textView = findViewById(R.id.verifyText);
+////                    textView.setText(verificationResult);
+//        } catch (Exception e) {
+//            Log.d(TAG, "fail to verify" + e.toString());
+////                    TextView textView = findViewById(R.id.verifyText);
+////                    textView.setText("fail to verify");
+//        }
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ByteArrayInputStream inputStream =
+                new ByteArrayInputStream(outputStream.toByteArray());
+
+        AsyncTask<InputStream, String, VerifyResult> detectTask =
+                new AsyncTask<InputStream, String, VerifyResult>() {
+                    String exceptionMessage = "";
+
+                    @Override
+                    protected VerifyResult doInBackground(InputStream... params) {
+                        try {
+//                            publishProgress("Detecting...");
+                            VerifyResult result = faceServiceClient.verify(selected, profilePhoto);
+                            if (result == null){
+                                publishProgress(
+                                        "Detection Finished. Nothing detected");
+                                return null;
+                            }
+//                            publishProgress(String.format(
+//                                    "Detection Finished. %d face(s) detected",
+//                                    result.length));
+                            return result;
+                        } catch (Exception e) {
+                            exceptionMessage = String.format(
+                                    "Detection failed: %s", e.getMessage());
+                            return null;
+                        }
+                    }
+
+//                    @Override
+//                    protected void onPreExecute() {
+//                        //TODO: show progress dialog
+//                        detectionProgressDialog.show();
+//                    }
+//                    @Override
+//                    protected void onProgressUpdate(String... progress) {
+//                        //TODO: update progress
+//                        detectionProgressDialog.setMessage(progress[0]);
+//                    }
+
+                    private static final String LOG_TAG = "LogActivity";
+
+
+                    protected void onPostExecute(Face[] result) {
+                        //TODO: update face frames
+//                        detectionProgressDialog.dismiss();
+
+//                        if(!exceptionMessage.equals("")){
+//                            showError(exceptionMessage);
+//                        }
+
+                        //TextView textView = findViewById(R.id.verifyText);
+                        //textView.setText(result[1].toString());
+                    }
+                };
+
+        try{
+            return detectTask.execute(inputStream).get().isIdentical;
+        }catch (Exception e){
+            return false;
+        }
     }
 
 }
--- app/src/main/java/com/example/unifriends/Login.java
+++ app/src/main/java/com/example/unifriends/Login.java
@@ -51,7 +51,6 @@ public class Login extends AppCompatActivity {
 
                         }
 
-                        // ...
                     }
                 });
     }
