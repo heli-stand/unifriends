50249ba42cb5bfe1413af356ed09abd70d6bbc98
Li He <jerelyhe@gmail.com>
2019-10-24 22:30:50 +1100

    ui fixed

--- app/src/main/java/com/example/unifriends/MainActivity.java
+++ app/src/main/java/com/example/unifriends/MainActivity.java
@@ -45,8 +45,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        FirebaseAuth.getInstance().signOut();
-//        Log.e(TAG, FirebaseAuth.getInstance().getUid());
         user = FirebaseAuth.getInstance().getCurrentUser();
         if ( user == null){
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
