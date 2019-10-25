cc78db74ed4a7bc264a9f5e0ef8e2fa6e4d60f9b
Chih-Kuan Wu <c.wu35@student.unimelb.edu.au>
2019-10-25 18:27:13 +1100

    FriendProfile photo fix

--- app/src/main/java/com/example/unifriends/friendFinder/FriendProfile.java
+++ app/src/main/java/com/example/unifriends/friendFinder/FriendProfile.java
@@ -1,8 +1,11 @@
 package com.example.unifriends.friendFinder;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,11 +16,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.unifriends.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.common.primitives.Ints;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import org.w3c.dom.Text;
 
@@ -25,6 +32,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FriendProfile extends AppCompatActivity {
+    private static final String TAG = "FriendProfile";
 
     private String[] intsStrings = {"Music", "Traveling", "Dancing"," Movies"," Reading"," Writing",
             " Going out"," Sports"," Gaming"," Blogging"," Foodie"," History"," Outdoors",
@@ -53,7 +61,7 @@ public class FriendProfile extends AppCompatActivity {
                 DocumentSnapshot user = task.getResult();
                 nametext.setText(user.getString("name"));
                 userName = user.getString("name");
-                image.setImageResource(R.drawable.alexis);
+                setPhoto(user.get("photo").toString(), image);
                 int[] intsArray = Ints.toArray((List<Integer>) user.get("interests"));
                 ArrayList<String> ints = new ArrayList<>();
                 for (int i = 0; i < intsArray.length; i++) {
@@ -69,6 +77,30 @@ public class FriendProfile extends AppCompatActivity {
 
 
     }
+
+    private void setPhoto(String source, final ImageView imageView){
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+
+        StorageReference pathReference = storageRef.child(source);
+
+        final long ONE_MEGABYTE = 1024 * 1024 * 5;
+        pathReference.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                // Data for "images/island.jpg" is returns, use this as needed
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                imageView.setImageBitmap(bitmap);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+                Log.d(TAG, exception.toString());
+            }
+        });
+    }
+
 
     public void goToMap(View view) {
 
